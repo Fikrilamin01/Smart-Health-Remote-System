5188aff89b247f544b975c9a6ca6d3297d03c221
fikri <muhammadfikri.a@graduate.utm.my>
2021-12-07 17:04:25 +0800

    Add SuccessHandler to control view page after user login

--- SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/WebConfiguration/SecurityConfiguration.java
+++ SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/WebConfiguration/SecurityConfiguration.java
@@ -3,14 +3,14 @@ package com.SmartHealthRemoteSystem.SHSR.WebConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -18,10 +18,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
     private MyUserDetailsService myUserDetailsService;
+    private AuthenticationSuccessHandler successHandler;
 
     @Autowired
-    public SecurityConfiguration(MyUserDetailsService myUserDetailsService){
+    public SecurityConfiguration(MyUserDetailsService myUserDetailsService, AuthenticationSuccessHandler successHandler){
         this.myUserDetailsService = myUserDetailsService;
+        this.successHandler = successHandler;
     }
 
     @Override
@@ -36,7 +38,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/doctor/**").hasAnyRole("ADMIN","DOCTOR")
                 .antMatchers("/patient/**").hasAnyRole("ADMIN","DOCTOR","PATIENT")
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login")
+                .successHandler(successHandler)
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login");
     }
 
 
