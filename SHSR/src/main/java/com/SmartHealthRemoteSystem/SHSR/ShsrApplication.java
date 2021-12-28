7b9c11bcbf2745b584390d1d2ce7ed13541c5d01
Az3HyperX <93169556+Az3HyperX@users.noreply.github.com>
2021-12-28 15:15:56 +0800

    Database part

--- SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/ShsrApplication.java
+++ SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/ShsrApplication.java
@@ -12,15 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShsrApplication implements CommandLineRunner{
 
-	private final HealthStatusRepository healthStatusRepository;
-	private final SensorDataRepository sensordatarepository;
-	private final PatientRepository patientRepository;
 
-	public ShsrApplication(HealthStatusRepository healthStatusRepository, SensorDataRepository sensordatarepository, PatientRepository patientRepository) {
-		this.healthStatusRepository = healthStatusRepository;
-		this.sensordatarepository = sensordatarepository;
-		this.patientRepository = patientRepository;
-	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShsrApplication.class, args);
@@ -29,8 +23,7 @@ public class ShsrApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		patientRepository.savePatient(new Patient( "kesh_1",  "rakesh", "123",  "012345690",  "PATIENT",  "",  "1275",  "sheesh"));
-		sensordatarepository.CreateTest("kesh_1");
+
 
 
 	}
