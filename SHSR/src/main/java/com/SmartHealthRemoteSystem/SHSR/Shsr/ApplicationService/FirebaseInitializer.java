2d18f6c4662d61b2ec9fe26ae7776040333145ea
fikri <muhammadfikri.a@graduate.utm.my>
2022-01-27 15:14:39 +0800

    Delete the firebase json and replace it with a another method to initialize the firestore client which is more secure and safe.
    Add GSON and Common IO dependency
    
    Add firebase credential information in application.properties
    
    Force the use of HTTPS in SecurityConfiguration and application.properties

--- SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/Shsr/ApplicationService/FirebaseInitializer.java
+++ SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/Shsr/ApplicationService/FirebaseInitializer.java
@@ -1,10 +1,14 @@
 package com.SmartHealthRemoteSystem.SHSR.Shsr.ApplicationService;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.gson.Gson;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -13,10 +17,35 @@ import java.io.InputStream;
 
 @Service
 public class FirebaseInitializer {
+    private final Environment environment;
+
+    public FirebaseInitializer(Environment environment) {
+        this.environment = environment;
+    }
 
     @PostConstruct
     private void initDB() throws IOException {
-        InputStream serviceAccount = this.getClass().getClassLoader().getResourceAsStream("./smarthealthcaresupport-firebase-adminsdk-5jy1z-6e2ef548bb.json");
+        FirebaseCredential firebaseCredential = new FirebaseCredential();
+        Gson gson = new Gson();
+
+        //private key
+        firebaseCredential.setType(environment.getRequiredProperty("FIREBASE_TYPE"));
+        String privateKey = environment.getRequiredProperty("FIREBASE_PRIVATE_KEY").replace("\\n", "\n");
+        firebaseCredential.setProject_id(environment.getRequiredProperty("FIREBASE_PROJECT_ID"));
+        firebaseCredential.setPrivate_key_id(environment.getRequiredProperty("FIREBASE_PRIVATE_KEY_ID"));
+        firebaseCredential.setPrivate_key(privateKey);
+        firebaseCredential.setClient_email(environment.getRequiredProperty("FIREBASE_CLIENT_EMAIL"));
+        firebaseCredential.setClient_id(environment.getRequiredProperty("FIREBASE_CLIENT_ID"));
+        firebaseCredential.setAuth_uri(environment.getRequiredProperty("FIREBASE_AUTH_URI"));
+        firebaseCredential.setToken_uri(environment.getRequiredProperty("FIREBASE_TOKEN_URI"));
+        firebaseCredential.setAuth_provider_x509_cert_url(environment.getRequiredProperty("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"));
+        firebaseCredential.setClient_x509_cert_url(environment.getRequiredProperty("FIREBASE_CLIENT_X509_CERT_URL"));
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = gson.toJson(firebaseCredential);
+
+        InputStream serviceAccount = IOUtils.toInputStream(jsonString);
+
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setDatabaseUrl("https://smarthealthcaresupport-default-rtdb.asia-southeast1.firebasedatabase.app")
