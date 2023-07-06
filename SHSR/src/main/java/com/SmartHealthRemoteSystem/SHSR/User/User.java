18ebae936b1aadbc057fda5d823069863d2c6686
Fikrilamin01 <muhammadfikri.a@graduate.utm.my>
2023-07-06 12:01:50 +0800

    Test jenkin jira web hook

--- SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/User/User.java
+++ SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/User/User.java
@@ -1,8 +1,8 @@
 package com.SmartHealthRemoteSystem.SHSR.User;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.List;
 
 public class User {
     private String userId;
